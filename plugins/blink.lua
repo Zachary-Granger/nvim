fd5a3f938540464b4aefb075604375cc3d79c552
Zachary Granger <zachary.g@hotmail.ca>
2025-11-16 22:02:05 -0800

    added blink autocomplete

--- plugins/blink.lua
+++ plugins/blink.lua
@@ -3,17 +3,19 @@ vim.pack.add({
 		src = 'https://github.com/saghen/blink.cmp',
 		dependencies = { 'rafamadriz/friendly-snippets' },
 		version = '1.*',
-
-		---@module 'blink.comp'
-		---@type blink.cmp.Config
-		opts = {
-			keymap = { preset = 'default' },
-			completion = { documentation = { auto_show = false } },
-			source = {
-				default = { 'lsp', 'path', 'snippets', 'buffer' },
-			},
-			fuzzy = { implementation = 'prefer_rust_with_warning' }
-		},
-		opts_extend = { 'sources.default' }
 	}
+})
+
+require('blink.cmp').setup({
+	keymap = { preset = 'default' },
+	appearance = {
+		nerd_font_variant = 'mono'
+	},
+	completion = {
+		documentation = { auto_show = true }
+	},
+	sources = {
+		default = { 'lsp', 'path', 'snippets', 'buffer' }
+	},
+	fuzzy = { implementation = 'prefer_rust_with_warning' }
 })
