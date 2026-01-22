bd2a57de3086534dba03ed520992368529abd80f
Zachary Granger <zachary.g@hotmail.ca>
2026-01-21 21:36:46 -0800

    Oil hidden files for Godot

--- plugins/oil.lua
+++ plugins/oil.lua
@@ -2,4 +2,11 @@ vim.pack.add({
 	{ src = 'https://github.com/stevearc/oil.nvim' }
 })
 
-require('oil').setup()
+require('oil').setup({
+	view_options = {
+		show_hidden = false,
+		is_hidden_file = function(name, _)
+			return vim.startswith(name, '.') or vim.endswith(name, ".uid")
+		end
+	}
+})
